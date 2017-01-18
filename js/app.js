306d77466d9bd21710b1440f7b4240b5c076e5c8
Tommy-Lim <tlim24@gmail.com>
2017-01-17 23:25:07 -0800

    finalized all lab requirements

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-/* setup your angular app here */
-
 angular.module('App', [])
 .controller('AppController', ['$scope', function($scope){
 
@@ -7,24 +5,78 @@ angular.module('App', [])
 
   $scope.fruits = [];
   $scope.vegetables = [];
-  $scope.items = fruits.concat(vegetables);
+  $scope.pool = fruits.concat(vegetables);
 
   // DECLARE FUNCTIONS
 
+  // RANDOMIZE ARRAY
   $scope.randomizeArray = function(arr){
-    console.log(arr);
-    for(index=0; index<arr.length; index++){
+    arr.forEach(function(item, index){
       arr.splice(Math.floor(Math.random()*arr.length), 0, arr.splice(index, 1)[0]);
-    }
+    });
     return arr;
   };
 
-  $scope.randomizeArray($scope.items);
+  // MOVE ITEM FROM FRUITS TO POOL
+  $scope.moveFromFruitsToPool = function(index){
+    $scope.pool.push($scope.fruits.splice(index, 1)[0]);
+  };
+
+  // MOVE ITEM FROM POOL TO FRUITS
+  $scope.moveFromPoolToFruits = function(index){
+    $scope.fruits.push($scope.pool.splice(index, 1)[0]);
+  };
+
+  // MOVE ITEM FROM POOL TO VEGETABLES
+  $scope.moveFromPoolToVegetables = function(index){
+    $scope.vegetables.push($scope.pool.splice(index, 1)[0]);
+  };
+
+  // MOVE ITEM FROM VEGETABLES TO POOL
+  $scope.moveFromVegetablesToPool = function(index){
+    $scope.pool.push($scope.vegetables.splice(index, 1)[0]);
+  };
+
+  // CHECK IF ITEM IN ORIGINAL FRUITS
+  $scope.inFruits = function(index){
+    if(fruits.indexOf($scope.fruits[index])>=0){
+      return true;
+    } else{
+      return false;
+    }
+  };
+
+  // CHECK IF ITEM IN ORIGINAL VEGETABLES
+  $scope.inVegetables = function(index){
+    if(vegetables.indexOf($scope.vegetables[index])>=0){
+      return true;
+    } else{
+      return false;
+    }
+  };
+
+  // CHECK IF ALL ITEMS IN CORRECT COLUMN
+  $scope.allCorrect = function(){
+    var result = true;
+    $scope.fruits.forEach(function(fruit){
+      if(vegetables.indexOf(fruit)>=0){
+        result = false;
+      } else{
+
+      }
+    });
+    $scope.vegetables.forEach(function(vegetable){
+      if(fruits.indexOf(vegetable)>=0){
+        result = false;
+      } else{
+
+      }
+    });
+    return result;
+  };
+
+  // RUN RANDOMIZE FRUITS AND VEGETABLES
+  $scope.randomizeArray($scope.pool);
 
 
 }]);
-
-// debug stuff to show the app is loading and fruit / veggies are available
-console.log('App Started');
-console.log('Fruit count', fruits.length);
-console.log('Veggie count', vegetables.length);
